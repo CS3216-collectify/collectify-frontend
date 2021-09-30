14772d2434c4ab3f711c94717897221f3fd45a40
teo-jun-xiong <teojunxiong97@gmail.com>
2021-09-30 17:50:26 +0800

    Implements #3

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,8 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from "@ionic/react";
-import ExploreContainer from "../components/ExploreContainer";
-import "./Home.css";
+import { IonPage } from "@ionic/react";
+import "./Home.scss";
 
 const Home = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
+  return <IonPage className="page"></IonPage>;
 };
 
 export default Home;
