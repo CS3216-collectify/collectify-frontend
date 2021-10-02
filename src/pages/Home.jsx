0b83eb453f72cb4a4c22ee4e184d8aec6b179b42
teo-jun-xiong <teojunxiong97@gmail.com>
2021-10-02 11:07:23 +0800

    Implement responsive navigation bars

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,124 +1,38 @@
-import { IonContent, IonPage, IonText, IonTitle, IonHeader, IonToolbar, IonGrid, IonRow, IonCol } from "@ionic/react";
+import { IonContent, IonPage, IonText, IonTitle, IonHeader, IonToolbar, IonGrid, IonRow, IonCol, IonButtons, IonButton, IonIcon } from "@ionic/react";
 import "./Home.scss";
+import { personCircle, search, helpCircle, star, create, ellipsisHorizontal, ellipsisVertical } from "ionicons/icons";
+import { homeOutline, searchOutline, addCircleOutline, chatbubblesOutline, personCircleOutline, settingsOutline} from "ionicons/icons";
 
 const Home = () => {
   return (
     <IonPage>
-         <IonToolbar className="ion-hide-sm-down">
-          <IonTitle>Title Only</IonTitle>
-        </IonToolbar>
- 
-      <IonContent  className="ion-padding">
-        {/* IonGrid fixes its max-width  */}
+      <IonToolbar className="ion-hide-sm-down">
+        <IonTitle>collectify</IonTitle>
+
+        <IonButtons slot="secondary">
+          <IonButton>
+            <IonIcon size="small" slot="icon-only" icon={search} />
+          </IonButton>
+          <IonButton>
+            <IonIcon size="small" slot="icon-only" icon={addCircleOutline} />
+          </IonButton>
+          <IonButton>
+            <IonIcon size="small" slot="icon-only" icon={chatbubblesOutline} />
+          </IonButton>
+          <IonButton>
+            <IonIcon size="small" slot="icon-only" icon={personCircleOutline} />
+          </IonButton>
+          <IonButton>
+            <IonIcon size="small" slot="icon-only" icon={settingsOutline} />
+          </IonButton>
+        </IonButtons>
+      </IonToolbar>
+
+      {/* Ion padding applies 16px  */}
+      <IonContent className="ion-padding">
+        {/* IonGrid with fixed property does not allow width to stretch in desktop */}
         <IonGrid fixed>
           <IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow>
-
-          <IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-            <IonCol>ion-col</IonCol>
-          </IonRow><IonRow>
             <IonCol>ion-col</IonCol>
             <IonCol>ion-col</IonCol>
             <IonCol>ion-col</IonCol>
