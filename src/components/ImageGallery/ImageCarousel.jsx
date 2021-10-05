c56b99ce3165f93489087cd1ade1f7dde8212a13
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-10-05 14:47:34 +0800

    Add padding to slide in carousel

--- src/components/ImageGallery/ImageCarousel.jsx
+++ src/components/ImageGallery/ImageCarousel.jsx
@@ -1,33 +1,33 @@
-import { IonContent, IonGrid, IonIcon, IonRow, IonSlide, IonSlides } from "@ionic/react";
+import { IonCol, IonContent, IonGrid, IonIcon, IonRow, IonSlide, IonSlides } from "@ionic/react";
 import { useState } from "react";
 import FlexIonImg from "../Image/FlexIonImg";
-import { ellipse } from "ionicons/icons";
+// import { ellipse } from "ionicons/icons";
 import "./styles.css";
 const sampleImage = "https://i1.wp.com/jejuhydrofarms.com/wp-content/uploads/2020/05/blank-profile-picture-973460_1280.png?ssl=1";
 const images = [sampleImage, sampleImage, sampleImage, sampleImage];
 
-const SlidesPager = (props) => {
-  const { activeIndex, length } = props;
+// const SlidesPager = (props) => {
+//   const { activeIndex, length } = props;
 
-  const isSelected = (idx) => idx === activeIndex;
-  const idxColor = (idx) => isSelected(idx) ? "#ffffff" : "#000000";
+//   const isSelected = (idx) => idx === activeIndex;
+//   const idxColor = (idx) => isSelected(idx) ? "#ffffff" : "#000000";
 
-  return (
-    <IonGrid>
-      <IonRow className="ion-align-items-center">
-        {
-          [...Array(length)].map((v, idx) => (
-            <IonIcon 
-              key={idx} 
-              icon={ellipse} 
-              color={idxColor(idx)}
-            />
-          ))
-        }
-      </IonRow>
-    </IonGrid>
-  )
-}
+//   return (
+//     <IonGrid>
+//       <IonRow className="ion-align-items-center">
+//         {
+//           [...Array(length)].map((v, idx) => (
+//             <IonIcon 
+//               key={idx} 
+//               icon={ellipse} 
+//               color={idxColor(idx)}
+//             />
+//           ))
+//         }
+//       </IonRow>
+//     </IonGrid>
+//   )
+// }
 
 const ImageCarousel = (props) => {
   const [activeIndex, setActiveIndex] = useState(0);
@@ -43,7 +43,9 @@ const ImageCarousel = (props) => {
     >
       {images.map((imgUrl, idx) => (
         <IonSlide key={idx}>
-          <FlexIonImg src={imgUrl}/>
+          <IonRow className="padding-bottom">
+            <FlexIonImg src={imgUrl} />
+          </IonRow>
         </IonSlide>
       ))}
     </IonSlides>
