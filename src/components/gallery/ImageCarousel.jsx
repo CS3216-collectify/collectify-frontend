83c69634c878a8d89c86bffff43084b78393fd03
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-10-07 03:14:45 +0800

    Remove comments

--- src/components/gallery/ImageCarousel.jsx
+++ src/components/gallery/ImageCarousel.jsx
@@ -1,33 +1,9 @@
 import { IonCol, IonContent, IonGrid, IonIcon, IonRow, IonSlide, IonSlides } from "@ionic/react";
 import { useState } from "react";
 import FlexImage from "../image/FlexImage";
-// import { ellipse } from "ionicons/icons";
 import "./styles.scss";
 const sampleImage = "https://i1.wp.com/jejuhydrofarms.com/wp-content/uploads/2020/05/blank-profile-picture-973460_1280.png?ssl=1";
 const images = [sampleImage, sampleImage, sampleImage, sampleImage];
-
-// const SlidesPager = (props) => {
-//   const { activeIndex, length } = props;
-
-//   const isSelected = (idx) => idx === activeIndex;
-//   const idxColor = (idx) => isSelected(idx) ? "#ffffff" : "#000000";
-
-//   return (
-//     <IonGrid>
-//       <IonRow className="ion-align-items-center">
-//         {
-//           [...Array(length)].map((v, idx) => (
-//             <IonIcon 
-//               key={idx} 
-//               icon={ellipse} 
-//               color={idxColor(idx)}
-//             />
-//           ))
-//         }
-//       </IonRow>
-//     </IonGrid>
-//   )
-// }
 
 const ImageCarousel = (props) => {
   const [activeIndex, setActiveIndex] = useState(0);
