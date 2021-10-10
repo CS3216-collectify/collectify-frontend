1df01c1b631fd0a3aa8fa1d5a9d83d4855b12a62
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-10-11 04:14:15 +0800

    Integrate collectionService in collectionpage

--- src/pages/collection/Collection.jsx
+++ src/pages/collection/Collection.jsx
@@ -1,9 +1,43 @@
-import { IonCol, IonContent, IonGrid, IonRow } from '@ionic/react';
-import ImageGrid from '../../components/gallery/ImageGrid'
-import "./Collection.scss"
+import { IonCol, IonContent, IonGrid, IonLoading, IonRow } from "@ionic/react";
+import { useEffect, useState } from "react";
+import { useParams } from "react-router";
+import ImageGrid from "../../components/gallery/ImageGrid";
+import { getCollectionByCollectionId } from "../../services/collections";
+import "./Collection.scss";
 
 const Collection = (props) => {
-  const { title = "Test Collection", ownerName = "Test", ownerUsername = "test", description = "Test Collection Description..." } = props;
+  // const { title = "Test Collection", ownerName = "Test", ownerUsername = "test", description = "Test Collection Description..." } = props;
+  const { collectionId } = useParams();
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [ownerName, setOwnerName] = useState("TODO");
+  const [ownerUsername, setOwnerUsername] = useState("todo");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    loadCollectionData();
+  }, []);
+
+  const loadCollectionData = async () => {
+    setLoading(true);
+    try {
+      const collectionData = await getCollectionByCollectionId(collectionId);
+      setTitle(collectionData.collectionName);
+      setDescription(collectionData.collectionDescription);
+      // setOwnerName("TODO");
+      // setOwnerUsername("todo");
+    } catch (e) {
+      console.log(e);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <IonLoading isOpen={loading} />;
+  }
+
   return (
     <IonContent>
       <IonGrid fixed>
@@ -17,7 +51,9 @@ const Collection = (props) => {
         </IonRow>
         <IonRow className="top-margin-s">
           <IonCol>
-            <p>by @{ownerUsername} ({ownerName}) </p>
+            <p>
+              by @{ownerUsername} ({ownerName}){" "}
+            </p>
           </IonCol>
         </IonRow>
         <IonRow className="top-margin-s">
@@ -28,7 +64,7 @@ const Collection = (props) => {
         <ImageGrid />
       </IonGrid>
     </IonContent>
-  )
-}
+  );
+};
 
 export default Collection;
