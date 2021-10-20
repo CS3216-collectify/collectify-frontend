0a7eab9f015a53df62ed57ee06949250202cb2dc
teo-jun-xiong <teojunxiong97@gmail.com>
2021-10-20 21:06:31 +0800

    Combine fetching useEffects in Profile

--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -30,7 +30,7 @@ const Profile = () => {
   // if not username and isLoggedIn, redirect to /profile/{username_from_local_storage}
   // if not username and not isLoggedIn, prompt log in
   let { username } = useParams();
- 
+
   // TODO : add profile description and pass to EditProfile
   const [profileUserId, setProfileUserId] = useState(null);
   const [profileFirstName, setProfileFirstName] = useState("");
@@ -89,15 +89,10 @@ const Profile = () => {
 
   useEffect(() => {
     if (!username && currentUserId) {
+      loadUserCollections();
       getUserInformation();
     }
-  }, [currentUserId, getUserInformation, username]);
-
-  useEffect(() => {
-    if (!username && currentUserId) {
-      loadUserCollections();
-    }
-  }, [currentUserId, loadUserCollections, username]);
+  }, [currentUserId, getUserInformation, loadUserCollections, username]);
 
   const fetchNextPage = () => {
     console.log("load next");
