dafc3bd850c1baf0e088e41a94c3d4aeda8c7242
teo-jun-xiong <teojunxiong97@gmail.com>
2021-10-23 22:10:20 +0800

    Refactor image carousel to slick

--- src/components/gallery/ImageCarousel.jsx
+++ src/components/gallery/ImageCarousel.jsx
@@ -1,18 +1,20 @@
-import { IonRow, IonSlide, IonSlides } from "@ionic/react";
 import FlexImage from "../image/FlexImage";
 import "./gallery.scss";
 
-import { Swiper, SwiperSlide } from "swiper/react";
-import "swiper/swiper-bundle.min.css";
-import "swiper/swiper.min.css";
-
-const slideOpts = {
-  initialSlide: 0,
-};
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const ImageCarousel = (props) => {
   const { imageUrls = [] } = props;
 
+  var settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
   return (
     // <IonSlides pager={true} options={slideOpts} className="image-carousel">
     //   {imageUrls.map((imgUrl, idx) => (
@@ -21,13 +23,32 @@ const ImageCarousel = (props) => {
     //     </IonSlide>
     //   ))}
     // </IonSlides>
-    <Swiper pagination={true} centeredSlides className="image-carousel">
+    // <Swiper pagination={true} centeredSlides className="image-carousel">
+    //   {imageUrls.map((imgUrl, idx) => (
+    //     <SwiperSlide key={idx}>
+    //       <FlexImage src={imgUrl} />
+    //     </SwiperSlide>
+    //   ))}
+    // </Swiper>
+    // !!imageUrls && (
+    //   <Carousel showThumbs={false} showStatus={false} swipeable={true} emulateTouch={true} className="image-carousel">
+    //     {imageUrls.map((imgUrl, idx) => (
+    //       <div className="image-carousel-image--container" key={idx} style={{ backgroundImage: `url(${imgUrl})` }}>
+    //         <FlexImage src={imgUrl} />
+    //       </div>
+    //     ))}
+    //   </Carousel>
+    // )
+
+    <Slider {...settings} className="image-carousel">
       {imageUrls.map((imgUrl, idx) => (
-        <SwiperSlide key={idx}>
-          <FlexImage src={imgUrl} />
-        </SwiperSlide>
+        <>
+          <div className="image-carousel-image--container" key={idx} style={{ backgroundImage: `url('${imgUrl}')` }}>
+            <FlexImage src={imgUrl} />
+          </div>
+        </>
       ))}
-    </Swiper>
+    </Slider>
   );
 };
 
