81042c66b48e8580f78fa1460c4bf5a5d6f84964
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-10-23 20:57:56 +0800

    Link user card to user profile page

--- src/components/user-list/UserCard.jsx
+++ src/components/user-list/UserCard.jsx
@@ -1,10 +1,16 @@
 import { IonAvatar, IonCol, IonImg, IonItem, IonLabel } from "@ionic/react";
+import { useHistory } from "react-router";
 
 const UserCard = (props) => {
+  const history = useHistory();
   const { pictureUrl, username } = props;
 
+  const clickHandler = () => {
+    history.push(`/profile/${username}`);
+  }
+
   return (
-    <IonItem>
+    <IonItem onClick={clickHandler}>
       <IonAvatar>
         <IonImg src={pictureUrl} />
       </IonAvatar>
