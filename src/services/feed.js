76ba99c2439199670c3418f6db660f3b5cf8cf9d
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-10-23 21:34:10 +0800

    Update getFeed

--- src/services/feed.js
+++ src/services/feed.js
@@ -1,13 +1,11 @@
 import server from "../utils/server";
 
-export const getFeed = async (offset, limit, followed = false) => {
+export const getFeed = async (offset, limit) => {
   const params = {
     offset,
     limit,
+    followed: true,
   };
-  if (followed) {
-    params.followed = true;
-  }
   console.log("GET /items... string query:", params);
   const res = await server.get("/items", { params });
   console.log(res);
