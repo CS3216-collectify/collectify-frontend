239e308ca45cdfca018ef8749cd94d4e4905b1b1
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-10-24 22:21:43 +0800

    Update save and delete buttons

--- src/components/button/DeleteButton.jsx
+++ src/components/button/DeleteButton.jsx
@@ -1,5 +1,6 @@
 import { IonButton, IonIcon } from "@ionic/react";
 import { trashOutline } from "ionicons/icons";
+import Text from "../text/Text";
 
 import './button.scss';
 
@@ -7,6 +8,7 @@ const DeleteButton = ({ onClick }) => {
   return (
     <IonButton size="small" color="danger" onClick={onClick}>
       <IonIcon size="small" slot="icon-only" icon={trashOutline} />
+      Delete
     </IonButton>
   );
 };
--- src/components/button/SaveButton.jsx
+++ src/components/button/SaveButton.jsx
@@ -1,12 +1,12 @@
-import { IonButton, IonLabel } from "@ionic/react";
+import { IonButton, IonIcon, IonLabel } from "@ionic/react";
+import { saveOutline } from "ionicons/icons";
 
 import "./button.scss";
 
 const SaveButton = ({ onClick: saveHandler }) => {
   return (
-    <IonButton
-      onClick={saveHandler}
-    >
+    <IonButton onClick={saveHandler}>
+      <IonIcon size="small" slot="icon-only" icon={saveOutline} />
       Save
     </IonButton>
   );
