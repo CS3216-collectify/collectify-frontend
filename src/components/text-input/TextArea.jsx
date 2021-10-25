441ca9655966fa8640134d149c8e840f42c7cb70
marcus2k <60178730+marcus2k@users.noreply.github.com>
2021-10-25 16:25:56 +0800

    Remove autoGrow from textarea

--- src/components/text-input/TextArea.jsx
+++ src/components/text-input/TextArea.jsx
@@ -4,7 +4,8 @@ const TextArea = ({ label, value, placeholder, onChange }) => {
   return (
     <>
       <IonLabel position="stacked">{label}</IonLabel>
-      <IonTextarea autoGrow value={value} placeholder={placeholder} onIonChange={(e) => onChange(e.detail.value)} />
+      {/* <IonTextarea autoGrow value={value} placeholder={placeholder} onIonChange={(e) => onChange(e.detail.value)} /> */}
+      <IonTextarea value={value} placeholder={placeholder} onIonChange={(e) => onChange(e.detail.value)} />
     </>
   );
 };
