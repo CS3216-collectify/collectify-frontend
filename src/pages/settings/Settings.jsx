773e4463261b36675edd390583439e93d5e87812
teo-jun-xiong <teojunxiong97@gmail.com>
2021-10-28 22:00:44 +0800

    Add mail to button

--- src/pages/settings/Settings.jsx
+++ src/pages/settings/Settings.jsx
@@ -1,45 +1,31 @@
-import { useState } from "react";
-import { IonContent, IonPage, IonGrid, IonRow, IonCol, IonItem, IonList } from "@ionic/react";
-import { useHistory } from "react-router-dom";
-
-import "./Settings.scss";
-import HomeToolbar from "../../components/toolbar/HomeToolbar";
-import LogoutButton from "../../components/button/LogoutButton";
-import useUserContext from "../../hooks/useUserContext";
-import useToastContext from "../../hooks/useToastContext";
+import { IonButton, IonContent, IonGrid, IonPage, IonRow } from "@ionic/react";
 import GoogleLoginButton from "../../components/button/GoogleLoginButton";
-import GoogleAuthStatus from "../../enums/google-auth-status.enum";
-import { getUserId } from "../../utils/auth/store";
+import LogoutButton from "../../components/button/LogoutButton";
 import Text from "../../components/text/Text";
+import HomeToolbar from "../../components/toolbar/HomeToolbar";
+import useUserContext from "../../hooks/useUserContext";
+import "./Settings.scss";
 
 // Pass user ID and load data\
 // some redirect if accessed by Guest
 const Settings = () => {
-  const { isUserAuthenticated, setIsUserAuthenticated } = useUserContext();
-  const setToast = useToastContext();
-  const history = useHistory();
-
-  const handleGoogleLogin = async (googleAuthStatus) => {
-    if (googleAuthStatus === GoogleAuthStatus.GOOGLE_AUTH_SUCCESS) {
-      // success
-      setToast({ message: "Google authentication successful!", color: "success" });
-      setIsUserAuthenticated(true);
-      history.replace("/home");
-    } else {
-      // error
-      setToast({ message: "Google authentication failed. Please try again.", color: "danger" });
-    }
-  };
+  const { isUserAuthenticated } = useUserContext();
 
   return (
-    <IonPage>
+    <IonPage className="settings">
       <HomeToolbar title="Settings" />
-
       {/* Ion padding applies 16px  */}
       <IonContent className="ion-padding">
         {/* IonGrid with fixed property does not allow width to stretch in desktop */}
 
         <IonGrid fixed>
+          <Text>Any feedback or suggestions? Please send us an email!</Text>
+          <IonRow className="mail-to-button--container">
+            <IonButton className="mail-to-button" href="mailto:cs3216collectify@gmail.com">
+              Contact us
+            </IonButton>
+          </IonRow>
+
           {!isUserAuthenticated && (
             <Text>
               <h1>Log in to collectify to begin showcasing your collectables to the world!</h1>
